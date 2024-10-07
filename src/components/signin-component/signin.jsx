28d92d90f2fdc6bd6cdb78aaf27a1dcb937d11d8
Toretto <145860512+Torettosaviour17@users.noreply.github.com>
2024-10-07 17:43:23 +0100

    just finished login in and signin responsiveness to my limits

--- src/components/signin-component/signin.jsx
+++ src/components/signin-component/signin.jsx
@@ -3,7 +3,6 @@ import { heroBackground } from "../../images";
 import { signgoogle, signfacebook, appleicon } from "../../images";
 import { Link } from "react-router-dom";
 
-
 const signin = () => {
   return (
     <div>
@@ -11,10 +10,10 @@ const signin = () => {
         style={{ backgroundImage: `url(${heroBackground})` }}
         className="bg-background1  w-full flex flex-row h-screen"
       >
-        <div className="w-1/2 h-[100%] hidden   opacity-80 sm:flex items-center justify-center bg-[green] text-white">
+        <div className="w-1/2 h-[100%] hidden   opacity-80 md:flex items-center justify-center bg-[green] text-white">
           {/* Adjusted opacity */}
           <div className="pl-28 pr-14">
-            <h1 className="text-[24px] leading-[40px] lg:text-[40px] font-bold pb-5">
+            <h1 className="text-[20px] leading-[40px] lg:text-[40px] font-bold pb-5">
               Your Logistics Partner For Seamless Delivery.
             </h1>
             <p className="text-2xl">
@@ -22,10 +21,10 @@ const signin = () => {
             </p>
           </div>
         </div>
-        <div className="w-full flex items-center justify-center sm:w-1/2 h-[100%] bg-[#D9D9D9]">
-          <div className="bg-white rounded-xl flex flex-col space-y-5 items-center justify-center h-[90%] min-w-[15rem] w-[25rem]">
+        <div className="w-full flex items-center justify-center md:w-1/2 h-[100%] bg-[#D9D9D9]">
+          <div className="bg-white rounded-xl flex flex-col py-5 items-center justify-center ">
             <div className="space-y-3 w-[90%] h-[90%]">
-              <h1 className="font-bold text-[30px] text-[#00A651]">
+              <h1 className="font-bold text-[25px] text-[#00A651]">
                 Create an account
               </h1>
               <p className="font-normal text-[20px] text-[#282828]">
@@ -35,9 +34,9 @@ const signin = () => {
                 <h1 className="font-semibold text-[18px] text-[#282828]">
                   Your name
                 </h1>
-                <div className="border-2 rounded-[5px] border-[#353535] w-[100%] h-[2.8rem]">
+                <div className=" rounded-[5px] border-[#353535] w-[100%] h-[2.8rem]">
                   <input
-                    className="w-full px-2 rounded-[5px] h-10 outline-none "
+                    className="w-full border text-sm lg:text-lg p-2"
                     type="text"
                     placeholder="First Last"
                   />
@@ -47,9 +46,9 @@ const signin = () => {
                 <h1 className="font-semibold text-[18px] text-[#282828]">
                   Email
                 </h1>
-                <div className="border-2 rounded-[5px] border-[#353535] w-[100%] h-[2.8rem] ">
+                <div className=" rounded-[5px] border-[#353535] w-[100%] h-[2.8rem] ">
                   <input
-                    className="w-full px-2 rounded-[5px] h-10 outline-none "
+                    className="w-full border text-sm lg:text-lg p-2"
                     type="text"
                     placeholder="you@email.com"
                   />
@@ -59,21 +58,20 @@ const signin = () => {
                 <h1 className="font-semibold text-[18px] text-[#282828]">
                   Password
                 </h1>
-                <div className="border-2 rounded-[5px] border-[#353535] w-[100%] h-[2.8rem]">
+                <div className=" border-[#353535] w-[100%] h-[2.8rem]">
                   <input
-                    className="w-full px-2 rounded-[5px] h-10 outline-none "
+                    className="w-full border text-sm lg:text-lg p-2"
                     type="text"
                     placeholder="By.Y0u02"
                   />
                 </div>
               </div>
-
               <p className="text-[#606060]">
                 By signing up I agree to the{" "}
                 <span className="font-normal text-[#00A651]">
                   terms & conditions
                 </span>{" "}
-                and
+                and{" "}
                 <span className="font-normal text-[#00A651]">
                   privacy policy
                 </span>
@@ -83,13 +81,13 @@ const signin = () => {
                   Create An Account
                 </button>
               </div>
-              <div className="flex items-center justify-center my-4">
+              <div className="flex items-center justify-center">
                 <div className="border-t border-gray-300 flex-grow"></div>
                 <span className="mx-4 text-gray-500">or</span>
                 <div className="border-t border-gray-300 flex-grow"></div>
               </div>
               <div className="h-16 w-full flex items-center flex-row justify-center gap-3">
-                <div className="h-10 w-[25%] flex items-center justify-center border-2">
+                <div className=" w-[25%] flex items-center justify-center border-2">
                   <img src={signgoogle} alt="" />
                 </div>
                 <div className="h-10 w-[25%]   flex items-center justify-center ">
@@ -101,11 +99,11 @@ const signin = () => {
               </div>
             </div>
             <div className="flex justify-center">
-                <p>Already a Member?</p>
-                <Link to={"/login"} className="text-[#00A651]">
-                  Sign in
-                </Link>
-              </div>
+              <p>Already a Member?</p>
+              <Link to={"/login"} className="text-[#00A651]">
+                Sign in
+              </Link>
+            </div>
           </div>
         </div>
       </div>
