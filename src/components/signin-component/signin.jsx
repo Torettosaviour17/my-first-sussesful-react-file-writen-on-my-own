2677515c670911c8a3e573fc02a59420acc74bf5
Toretto <145860512+Torettosaviour17@users.noreply.github.com>
2024-10-07 11:38:47 +0100

    i just finshed reponsveness

--- src/components/signin-component/signin.jsx
+++ src/components/signin-component/signin.jsx
@@ -1,109 +1,114 @@
 import React from "react";
-import { heroBackground } from "../../images/index";
-// import { google } from "../../images/index";
-import { signfacebook } from "../../images/index";
-import { signgoogle } from "../../images/index";
+import { heroBackground } from "../../images";
+import { signgoogle, signfacebook, appleicon } from "../../images";
 import { Link } from "react-router-dom";
-import { appleicon } from "../../images/index";
+
 
 const signin = () => {
   return (
     <div>
-      <section
+      <div
         style={{ backgroundImage: `url(${heroBackground})` }}
-        className="relative flex object-cover justify-center items-center h-[100vh] w-full bg-cover bg-center"
+        className="bg-background1  w-full flex flex-row h-screen"
       >
-        <div className="absolute inset-0 bg-black/60"></div>{" "}
-        <div className="relative flex w-full h-full">
-          <div className="bg-[#00A651]/70 text-[#FFFFFF] w-1/2 h-full flex justify-center items-center">
-            <div className="pl-28 pr-14">
-              <h1 className="text-4xl font-bold pb-5">
-                Your Logistics Partner For Seamless Delivery.
-              </h1>
-              <p className="text-2xl">
-                We provide services for all your nationwide shipping needs.
-              </p>
-            </div>
-          </div>
-          <div className="bg-[#D9D9D9] w-1/2 h-full flex justify-center items-center">
-            <div className="bg-white rounded-xl py-10 px-10">
-              <h1 className="text-3xl font-bold text-[#00A651] py-4">
-                Create an account
-              </h1>
-              <p className="text-xl pb-7">
-                <b>Provide your details</b>
-              </p>
-              <form>
-                <label htmlFor="name">
-                  <b>Your name</b>
-                </label>
-                <br />
-                <input
-                  type="text"
-                  name="name"
-                  id="mail"
-                  className="w-full border text-lg p-2"
-                  placeholder="First Name"
-                />
-                <br />
-                <br />
-                <label htmlFor="mail">
-                  <b>Email</b>
-                </label>
-                <br />
-                <input
-                  type="email"
-                  name="mail"
-                  id="mail"
-                  className="w-full border text-lg p-2"
-                  placeholder="you@email.com"
-                />
-                <br />
-                <br />
-                <label htmlFor="password">
-                  <b>password</b>
-                </label>
-                <br />
-                <input
-                  type="text"
-                  className="w-full border text-lg p-2"
-                  id="password"
-                  placeholder="By.Y0u02"
-                />
-                <br />
-                <div className="flex justify-between text-md py-3">
-                  {/* <div>
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="">Remember me</label>
-                  </div>
-                  <div>
-                    <p>Forgot Password?</p>
-                  </div> */}
-                  <p>
-                    By signing up I agree to the{" "}
-                    <i className="text-[#00A651]">terms & conditions</i> and{" "}
-                    <i className="text-[#00A651]">privacy policy</i>
-                  </p>
-                </div>
-                <button className="w-full items-center bg-[#00A651] text-white rounded-xl py-3 text-lg font-bold">
-                  Create An Account
-                </button>
-              </form>
-              <div className="flex justify-between p-5">
-                <img src={signgoogle} alt="" />
-                <img src={signfacebook} alt="" />
-                <img src={appleicon} alt="" />
-              </div>
-              <div className="flex justify-center">
-                <p>Already a Member?</p>
-                <Link to={"/login"} className="text-[#00A651]">
-                  Login in.
-                </Link>
-              </div>
-            </div>
+        <div className="w-1/2 h-[100%] hidden   opacity-80 sm:flex items-center justify-center bg-[green] text-white">
+          {/* Adjusted opacity */}
+          <div className="pl-28 pr-14">
+            <h1 className="text-[24px] leading-[40px] lg:text-[40px] font-bold pb-5">
+              Your Logistics Partner For Seamless Delivery.
+            </h1>
+            <p className="text-2xl">
+              We provide services for all your nationwide shipping needs.
+            </p>
           </div>
         </div>
-      </section>
+        <div className="w-full flex items-center justify-center sm:w-1/2 h-[100%] bg-[#D9D9D9]">
+          <div className="bg-white rounded-xl flex flex-col space-y-5 items-center justify-center h-[90%] min-w-[15rem] w-[25rem]">
+            <div className="space-y-3 w-[90%] h-[90%]">
+              <h1 className="font-bold text-[30px] text-[#00A651]">
+                Create an account
+              </h1>
+              <p className="font-normal text-[20px] text-[#282828]">
+                Provide your details.
+              </p>
+              <div className="">
+                <h1 className="font-semibold text-[18px] text-[#282828]">
+                  Your name
+                </h1>
+                <div className="border-2 rounded-[5px] border-[#353535] w-[100%] h-[2.8rem]">
+                  <input
+                    className="w-full px-2 rounded-[5px] h-10 outline-none "
+                    type="text"
+                    placeholder="First Last"
+                  />
+                </div>
+              </div>
+              <div className="">
+                <h1 className="font-semibold text-[18px] text-[#282828]">
+                  Email
+                </h1>
+                <div className="border-2 rounded-[5px] border-[#353535] w-[100%] h-[2.8rem] ">
+                  <input
+                    className="w-full px-2 rounded-[5px] h-10 outline-none "
+                    type="text"
+                    placeholder="you@email.com"
+                  />
+                </div>
+              </div>
+              <div className="">
+                <h1 className="font-semibold text-[18px] text-[#282828]">
+                  Password
+                </h1>
+                <div className="border-2 rounded-[5px] border-[#353535] w-[100%] h-[2.8rem]">
+                  <input
+                    className="w-full px-2 rounded-[5px] h-10 outline-none "
+                    type="text"
+                    placeholder="By.Y0u02"
+                  />
+                </div>
+              </div>
+
+              <p className="text-[#606060]">
+                By signing up I agree to the{" "}
+                <span className="font-normal text-[#00A651]">
+                  terms & conditions
+                </span>{" "}
+                and
+                <span className="font-normal text-[#00A651]">
+                  privacy policy
+                </span>
+              </p>
+              <div className=" w-full flex justify-center h-3rem">
+                <button className="h-[3rem] w-full rounded-lg bg-[#00A651] text-white">
+                  Create An Account
+                </button>
+              </div>
+              <div className="flex items-center justify-center my-4">
+                <div className="border-t border-gray-300 flex-grow"></div>
+                <span className="mx-4 text-gray-500">or</span>
+                <div className="border-t border-gray-300 flex-grow"></div>
+              </div>
+              <div className="h-16 w-full flex items-center flex-row justify-center gap-3">
+                <div className="h-10 w-[25%] flex items-center justify-center border-2">
+                  <img src={signgoogle} alt="" />
+                </div>
+                <div className="h-10 w-[25%]   flex items-center justify-center ">
+                  <img className="" src={signfacebook} alt="" />
+                </div>
+                <div className="h-10 w-[25%]   flex items-center justify-center ">
+                  <img src={appleicon} alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center">
+                <p>Already a Member?</p>
+                <Link to={"/login"} className="text-[#00A651]">
+                  Sign in
+                </Link>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
