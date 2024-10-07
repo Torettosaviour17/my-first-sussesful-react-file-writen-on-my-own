4fc126a97aa758ad083b3dc427a06b4c01311051
Toretto <145860512+Torettosaviour17@users.noreply.github.com>
2024-10-07 17:58:33 +0100

    working on welome page

--- src/components/welcome-component/welcome.jsx
+++ src/components/welcome-component/welcome.jsx
@@ -3,25 +3,27 @@ import OurService from "../homepage-components/ourService";
 import Footer from "../layout/footer"; // Adjusted path if necessary
 // import { Link } from "react-router-dom"; // Remove if not used
 import { heroBackground } from "../../images/index";
+import NavBar from "../layout/nav";
 
 const Welcome = () => {
   return (
     <div>
+      <NavBar />
       <section>
         <header
           style={{ backgroundImage: `url(${heroBackground})` }}
           className="bg-[#00A651] h-[100vh] w-full flex justify-center items-center text-center"
         >
-          <div className="p-20">
+          <div className="md:p-20">
             <div className="flex justify-center items-center">
-              <div className="text-[#00A651] bg-white text-3xl font-bold rounded-xl py-5 w-[50%]">
+              <div className="text-[#00A651] bg-white text-xl md:text-2xl font-bold rounded-xl py-5 w-[85%] md:w-[50%]">
                 Hi, John Austin! 👋
               </div>
             </div>
-            <h1 className="text-5xl font-bold text-white py-9">
+            <h1 className="text-2xl md:text-4xl font-bold text-white py-9">
               We Are Your Logistics Partner For Seamless Delivery.
             </h1>
-            <p className="text-white text-2xl">
+            <p className="text-white text-md md:text-2xl">
               We provide services for all your nationwide shipping needs.
             </p>
           </div>
